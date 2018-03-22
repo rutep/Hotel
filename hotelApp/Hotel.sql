f1af118c9c656de37b79b250187158b29e710e06
Pétur <rutep>
2018-03-22 13:13:34 +0000

    update

---
+++ hotelApp/Hotel.sql
@@ -0,0 +1,29 @@
+create table Hotel(
+    ssn int,
+    name String,
+    fylter1 String,
+    fylter2 String,
+    stars int
+);
+
+
+insert into Hotel  values (1, 'Brachytheciaceae', 'true', 'false', 3);
+insert into Hotel  values (2, 'Capparaceae', 'true', 'false', 5);
+insert into Hotel  values (3, 'Poaceae', 'false', 'true', 5);
+insert into Hotel  values (4, 'Poaceae', 'false', 'true', 5);
+insert into Hotel  values (5, 'Poaceae', 'true', 'true', 3);
+insert into Hotel  values (6, 'Flacourtiaceae', 'false', 'false', 3);
+insert into Hotel  values (7, 'Myoporaceae', 'false', 'true', 3);
+insert into Hotel  values (8, 'Lamiaceae', 'true', 'false', 5);
+insert into Hotel  values (9, 'Cactaceae', 'false', 'true', 4);
+insert into Hotel  values (10, 'Piperaceae', 'false', 'true', 3);
+insert into Hotel  values (11, 'Physciaceae', 'false', 'false', 2);
+insert into Hotel  values (12, 'Grossulariaceae', 'true', 'true', 2);
+insert into Hotel  values (13, 'Nyctaginaceae', 'false', 'false', 4);
+insert into Hotel  values (14, 'Selaginellaceae', 'false', 'true', 5);
+insert into Hotel  values (15, 'Asteraceae', 'true', 'true', 4);
+insert into Hotel  values (16, 'Arthoniaceae', 'true', 'false', 2);
+insert into Hotel  values (17, 'Rutaceae', 'true', 'false', 4);
+insert into Hotel  values (18, 'Cucurbitaceae', 'true', 'true', 1);
+insert into Hotel  values (19, 'Pyrenulaceae', 'true', 'false', 4);
+insert into Hotel  values (20, 'Cyperaceae', 'false', 'true', 5);
---
+++ schema.sql
@@ -0,0 +1,22 @@
+
+CREATE TABLE Hotel(
+  name string,
+  ssn int,
+  address string,
+  town string,
+  vegan int,
+  swimmingPool int,
+  breakfast int,
+  spa int,
+  handicappedAcc int,
+  stars int,
+  carParking int,
+  freeWifi int,
+  gym int,
+  doubleBed int,
+  singleBed int,
+  hotelSuite int,
+  singlePrice float,
+  doublePrice float,
+  suitePrice float
+);
