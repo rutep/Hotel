b1d2ce6e27d938170e37d3d46c7822f7ed20a2db
einargudnig <einargudnig@gmail.com>
2018-04-08 20:53:25 +0000

    Gerði nýjan Results clasa, hinn var ekki 'windowbuilder', sem ætti að sýna okkur niðurstöður

--- hotelApp/src/hotelApp/Results.java
+++ hotelApp/src/hotelApp/Results.java
@@ -1,7 +1,42 @@
 package hotelApp;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
 
-public class Results {
+public class Results extends JFrame {
 
-	private JFrame frame;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Results frame = new Results();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Results() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
+
 }
